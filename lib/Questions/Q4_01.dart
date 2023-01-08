93f3f243c5b59b99d22464a37ea9d38971851bce
motto11 <liujimmy7@gmail.com>
2023-01-09 02:13:54 +0800

    Update Q4_01.dart

--- lib/Questions/Q4_01.dart
+++ lib/Questions/Q4_01.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 import '../global.dart';
+import 'package:old_test/library/socket_stt.dart';
+import 'package:old_test/library/sound_recorder.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 class Question4Page extends StatefulWidget {
   const Question4Page({super.key});
@@ -11,8 +17,77 @@ class Question4Page extends StatefulWidget {
 
 class _Question4PageState extends State<Question4Page> {
 
-
+  final recorder = SoundRecorder();
   TextEditingController recognitionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    AudioPlayer().play(AssetSource('Voice/Q4_1.mp3'));
+    recorder.init();
+  }
+
+  @override
+  void dispose() {
+    recorder.dispose();
+    super.dispose();
+  }
+
+  Widget buildRecord() {
+    // whether is recording
+    final isRecording = recorder.isRecording;
+    // if recording => icon is Icons.stop
+    // else => icon is Icons.mic
+    final icon = isRecording ? Icons.stop : Icons.mic;
+    // if recording => color of button is red
+    // else => color of button is white
+    final primary = isRecording ? Colors.red : Colors.white;
+    // if recording => text in button is STOP
+    // else => text in button is START
+    final text = isRecording ? '停止錄音' : '開始錄音';
+    // if recording => text in button is white
+    // else => color of button is black
+    final onPrimary = isRecording ? Colors.white : Colors.black;
+
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+        // 設定 Icon 大小及屬性
+        foregroundColor: onPrimary,
+        backgroundColor: primary,
+        minimumSize: const Size(175, 50),
+      ),
+      icon: Icon(icon),
+      label: Text(
+        text,
+        // 設定字體大小及字體粗細（bold粗體，normal正常體）
+        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      // 當 icon 被點擊時執行的動作
+      onPressed: () async {
+        // getTemporaryDirectory(): 取得暫存資料夾，這個資料夾隨時可能被系統或使用者操作清除
+        Directory tempDir = await path_provider.getTemporaryDirectory();
+        // define file directory
+        String path = '${tempDir.path}/SpeechRecognition.wav';
+        // 控制開始錄音或停止錄音
+        await recorder.toggleRecording(path);
+        // When stop recording, pass wave file to socket
+        if (!recorder.isRecording) {
+          await Speech2Text().connect(path, setTxt, "MTK_ch");
+        }
+        // set state is recording or stop
+        setState(() {
+          recorder.isRecording;
+        });
+      },
+    );
+  }
+
+  void setTxt(taiTxt) {
+    setState(() {
+      recognitionController.text = taiTxt;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,15 +123,7 @@ class _Question4PageState extends State<Question4Page> {
                       ),
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/profile/age');
-                    },
-                    style: ElevatedButton.styleFrom(
-                        textStyle: const TextStyle(
-                            fontSize: 80, fontWeight: FontWeight.bold)),
-                    child: const Text("開始錄音"),
-                  )
+                  buildRecord()
                 ])));
   }
 }
